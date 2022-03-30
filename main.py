5333cf34c2dba50d216a0e0dfafdcb05ce224aa1
Xelios26 <ilijasavovic27@gmail.com>
2022-03-31 01:17:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import allure
 
 # global vars
 
 yahoo_xpath = '//*[@id="ybar-logo"]/img[1]'
 
-
-def check_yahoo_logo() -> bool:
-    try:
-        chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        chrome.get("https://www.yahoo.com/")
-        chrome.find_element(By.XPATH, yahoo_xpath)
-        print("Yahoo logo loaded successfully")
-        return True
-    except Exception as e:
-        print(f"Something went wrong: {e}")
-        return False
+@pytest.fixture()
+def test_setup():
+    global chrome
+    chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield
+    chrome.quit()
 
 
-def test_yahoo_logo():
-    result = check_yahoo_logo()
-    assert result
+
+@allure.description("This test checks if Yahoo logo is loaded and should pass")
+@allure.severity(severity_level="CRITICAL")
+def test_yahoo_logo(test_setup):
+    chrome.get("https://www.yahoo.com/")
+    assert chrome.find_element(By.XPATH, yahoo_xpath), "Yahoo logo not loaded"
+    chrome.close()
+
+
+
+
+
+
+
